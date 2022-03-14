fd06ef813af1720f93444cdf47f6a7cbdd094fec
Dungpi <phamdung.080602@gmail.com>
2022-03-14 22:20:12 +0700

    hihi

---
+++ school/src/baitap3_PTTKTT/package-info.java
@@ -0,0 +1,1 @@
+package baitap3_PTTKTT;
--- school/src/practice/HelloWorld.java
+++ school/src/practice/HelloWorld.java
@@ -2,6 +2,11 @@ package practice;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World !");
+//		System.out.println("Hello World !");
+		if(false || false || true || false){
+		    System.out.println("true");
+		}else{
+		    System.out.println("false");
+		}
 	}
 }
